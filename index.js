9ce974ec39e45b60a63f32ef81c1fd5a3fe3c6ad
dlamet <dlamet@yahoo.com>
2022-09-02 15:59:39 -0700

    958500710 Use rights flag instead of string

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const allRightsParentObject = require('./rights')
 
 /**
  * Check if user has right or not.
+ * This is a bitwise operation.  If the
  * Based on second params, it first get the respective bit value of right and then check if user has right or not.
- * @param {Object} token - token of user that contains the actual rights of a user in bitfield.
- * @param {string} userRightToCheck  - right to check if user has it or not. ex: "MANAGE_USERS"
+ * @param {number} token - number representing the bits of the rights the user can perform
+ * @param {number} rightToCheck  - right to check if user has it or not. ex: "MANAGE_USERS"
  * @returns true/false - true if user has right, false if user doesn't have right.
  */
-const hasRight = (token, userRightToCheck) => {
-  console.log(token, userRightToCheck)
-  const allRights = allRightsParentObject.allRights
-  return (
-    (allRights[userRightToCheck].flag & token.userRightsBits) ==
-    allRights[userRightToCheck].flag
-  )
+
+const hasRight = (token, rightToCheck) => {
+  console.log('hasRight: ', token, rightToCheck)
+  return (token & rightToCheck) > 0
 }
 
 module.exports = { allRights: allRightsParentObject, hasRight }
